844d9cf19f699d3d5030b435d48fa5d971e15f5e
Norent Khy <norent.dev@gmail.com>
2023-07-24 22:16:21 +0200

    serve hola mundo to web

--- main.tf
+++ main.tf
@@ -3,10 +3,39 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami =	"ami-0d527b8c289b4af7f"
-  instance_type = "t2.micro"
+  ami                    = "ami-0d527b8c289b4af7f"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hola Mundo~!" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
